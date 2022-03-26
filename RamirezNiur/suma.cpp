cb8ddaa5fde2dfe8ca815441d141855addc4a4d3
* <niurka.ramirez.castro@utelvt.edu.ec>
2022-03-26 13:59:43 -0500

    Subido desde mi kalley

---
+++ RamirezNiur/suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;                              int main()
+{
+float A,B,C;
+cout<<"Ingrese los valores de A B: ";
+cin>>A>>B;
+C=A+B;
+cout<<"El resultado es : ";
+cout<<C<<endl;
+
+return 0;
+
+
+
+        }
