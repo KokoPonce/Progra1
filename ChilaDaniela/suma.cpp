c0e21df41a5f2d4c68f23a8dd3e09e6ad2beb662
Chila Melany <daniela.chila.rosales@utelvt.edu.ec>
2022-03-28 01:10:19 -0500

    2da actualizacion desde mi celular

--- ChilaDaniela/suma.cpp
+++ ChilaDaniela/suma.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main(){
 	float A,B,C;
+	cout<<"Ingrese los numeros A B: ";
 	cin>>A>>B;
 	C=A+B;
 	cout<<C;
+	cout<<"El resultado de su suma es: ";
+	cout<<C<<endl;
 	return 0;
 }
