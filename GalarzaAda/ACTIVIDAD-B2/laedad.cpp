f6cb9a29d722c9955d6290f1dfc9183d49a7e8a7
AditaGalarza <ada.galarza.caicedo@utelvt.edu.ec>
2022-05-02 10:28:24 -0500

    Modifique desee mi A11

--- GalarzaAda/ACTIVIDAD-B2/laedad.cpp
+++ GalarzaAda/ACTIVIDAD-B2/laedad.cpp
@@ -3,8 +3,22 @@ using namespace std;
 int main()
 {
 	int aa,ma,da,an,mn,dn,a,m,d;
-	cout<<"ingrese fecha actual y su fecha de nacimiento:";
-	cin>>aa>>ma>>da>>an>>mn>>dn;
+	cout<<"Ingrese  su año de nacimiento:"<<endl;
+	cin>>aa;
+	cout<<"Ingrese su mes de nacimiento:"<<endl;
+	cin>>ma;
+	cout<<"Ingrese su día de nacimiento:"<<endl;
+	cin>>da;
+
+	cout<<endl;
+	cout<<endl;
+
+        cout<<"Ingrese el año actual:"<<endl;
+	cin>>an;
+	cout<<"Ingrese el mes actual:"<<endl;
+	cin>>mn;
+	cout<<"Ingrese el día actual:"<<endl;
+	cin>>dn;
 	if(da>=dn)
 	{
 		d=da-dn;
@@ -24,7 +38,7 @@ int main()
 		m=ma-mn;
 	}
 	a=aa-an;
-	cout<<"usted tiene"<<a<<"anos"<<endl;
+	cout<<"usted tiene"<<a<<"años"<<endl;
 	cout<<"con"<<m<<"meses"<<endl;
 	cout<<"y dias"<<d<<endl;
 	return 0;
