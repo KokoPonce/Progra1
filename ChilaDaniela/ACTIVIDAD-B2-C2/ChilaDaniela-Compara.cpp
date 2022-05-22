25e5509c387bf9aba918c32fd1b497a976381fc7
Aldo Carvajal <aldo.carvajal.klinger@utelvt.edu.ec>
2022-05-21 23:39:29 -0500

    Actualizacion de compara B2-C2 desde mi pc

--- ChilaDaniela/ACTIVIDAD-B2-C2/ChilaDaniela-Compara.cpp
+++ ChilaDaniela/ACTIVIDAD-B2-C2/ChilaDaniela-Compara.cpp
@@ -1,22 +1,33 @@
+//=======Nombre del programa: Compara de dos numeros
+//====Archivo: ChilaDaniela-Compara.cpp
+//====Autor: Chila Daniela
+//====Fecha de elaboracion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
 #include<iostream>
 using namespace std;
 int main()
 {
-	float A,B;
-	cout<<"ingrese A:";
-	cin>>A;
-	cout<<"ingrese B:";
-	cin>>B;
-	if(A==B) {
-	cout<<"El valor de A= "<<A<<" es igual a B "<<B<<endl;
+	float CD_A,CD_B;
+	cout<<"ingrese CD_A:";
+	cin>>CD_A;
+	cout<<"ingrese CD_B:";
+	cin>>CD_B;
+	if(CD_A==CD_B) {
+	cout<<"El valor de CD_A= "<<CD_A<<" es igual a CD_B "<<CD_B<<endl;
 
 	}else{
-		if(A<B){
+		if(CD_A<CD_B){
 
-	cout<<"El valor de A=" <<A<<" es menor a B="<<B<<endl;
+cout<<"//=======Nombre de programa: Compara de dos numeros"<<endl;
+cout<<"//====Archivo: ChilaDaniela-Compara.cpp"<<endl;
+cout<<"//====Autor: Chila Daniela "<<endl;
+cout<<"//====Fecha de elaboracion: 03-05-2022"<<endl;
+cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
+
+	cout<<"El valor de CD_A=" <<CD_A<<" es menor a CD_B="<<CD_B<<endl;
 		}else{
-	cout<<"El valor de B="<<B<<" es menor a A="<<A<<endl;
+	cout<<"El valor de CD_B="<<CD_B<<" es menor a CD_A="<<CD_A<<endl;
 		}
-	}
+		}
 	return 0;
-}
+	}
