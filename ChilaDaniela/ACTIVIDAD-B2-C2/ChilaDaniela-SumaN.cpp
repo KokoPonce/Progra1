e057f878ab63b03bb30a9e101ebe0ab05450ac7c
ChilaDaniela <daniela.chila.rosales@utelvt.edu.ec>
2022-05-22 01:51:02 -0500

    Actualizacion de Suma de varios numeros B2-C2 desde mi celular

--- ChilaDaniela/ACTIVIDAD-B2-C2/ChilaDaniela-SumaN.cpp
+++ ChilaDaniela/ACTIVIDAD-B2-C2/ChilaDaniela-SumaN.cpp
@@ -1,19 +1,32 @@
+//=======Nombre del programa: Suma de dos numeros
+//====Archivo: ChilaDaniela-SumaN.cpp
+//====Autor: Chila Daniela
+//====Fecha de elaboracion: 03-05-2022
+//====Fexha de ultima actualizacion: 19-05-2022
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	int c=0, n;
-	float s=0, x;
+	int CD_c=0, CD_n;
+	float CD_s=0, CD_x;
 	cout<<"Ingrese la cantidad de numeros a sumar : ";
-	cin>>n;
+	cin>>CD_n;
 	do{
          
 		cout<<"Ingrese un numero :";
-		cin>>x;
-		c=c+1;
-		s=s+x;
-	}while(c<n);
-	cout<<"La suma de los numeros es :"<<s<<endl;
+		cin>>CD_x;
+		CD_c=CD_c+1;
+		CD_s=CD_s+CD_x;
+	}while(CD_c<CD_n);
+
+cout<<"//=======Nombre del programa: Suma de dos numeros"<<endl;
+cout<<"//====Archivo: ChilaDaniela-SumaN.cpp"<<endl;
+cout<<"//====Autor: Chila Daniela"<<endl;
+cout<<"//====Fecha de elaboracion: 03-05-2022"<<endl;
+cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
+
+	cout<<"La suma de los numeros es :"<<CD_s<<endl;
 	return(0);
 }
